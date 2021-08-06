7a623f7a1fe6cb6cd78aec92a77c075122992057
Evgeniy Kurilenko <kurilenko.e.m@gmail.com>
2021-08-06 12:40:00 +0300

    initial commit

---
+++ src/main/java/com/burlywoodpalette/bwvideo/BWVideoApplication.java
@@ -0,0 +1,13 @@
+package com.burlywoodpalette.bwvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BWVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BWVideoApplication.class, args);
+	}
+
+}
